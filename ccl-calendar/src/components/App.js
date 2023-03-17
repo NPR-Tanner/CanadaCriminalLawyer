2a00f373a5ab476d315b1b1dfd11a06df9f0bac1
Tanner Olheiser <tolheiser@outlook.com>
2023-03-17 16:26:54 -0600

    Removed default p element

--- ccl-calendar/src/components/App.js
+++ ccl-calendar/src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './../App.css';
 import NavigationBar from './Navbar'; // import react component
 
 function App() {
@@ -7,9 +6,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <NavigationBar />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. I have just made an edit.
-        </p>
       </header>
     </div>
   );
