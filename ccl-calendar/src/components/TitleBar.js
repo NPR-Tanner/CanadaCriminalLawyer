c925f3ecda70fb0b36e3d749ac4dfb508e62da5c
Tanner Olheiser <tolheiser@outlook.com>
2023-03-17 14:30:58 -0600

    Added the elements and assets

--- ccl-calendar/src/components/TitleBar.js
+++ ccl-calendar/src/components/TitleBar.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import './../css/TitleBar.css'
+import './../css/TitleBar.css';
+import Instagram from './../assets/instagram-hdr.png'
+import YouTube from './../assets/youtube-hdr.png'
+import LinkedIn from './../assets/linkedin-hdr.png'
 
 const TitleBar = () => {
     return (
-        <section className="flex-row">
+        <section className="flex-row" id="titlebar">
             <div className="titleCTA">
-                <p className="titleType">Talk to a Lawyer Day or Night 24/7 <button className="titleButton">Call Now</button></p>
+                <p className="titleType">Talk to a Lawyer Day or Night 24/7 <button className="titleButton"><a href="tel:8555851777">Call Now</a></button></p>
             </div>
+
             <div className="titleSocials">
                 <ul>
-                    <li>Instagram Icon</li>
-                    <li>YouTube Icon</li>
-                    <li>LinkedIn Icon</li>
+                    <li><a href="https://www.instagram.com/canadacriminallawyer"><img src={Instagram} alt="Canada Criminal Lawyer Instagram" width="23px" /></a></li>
+                    <li><a href="https://www.youtube.com/channel/UCABALDoDJE9Aqj9kSpxMQyg"><img src={YouTube} alt="Canada Criminal Lawyer YouTube" width="29px" /></a></li>
+                    <li><a href="https://www.linkedin.com/company/42823021"><img src={LinkedIn} alt="Canada Criminal Lawyer LinkedIn" width="25px" /></a></li>
                     <li>Licensed Criminal Defence Lawyers Across Canada - <a href="tel:8555851777">(855) 585-1777</a></li>
                 </ul>
             </div>  
